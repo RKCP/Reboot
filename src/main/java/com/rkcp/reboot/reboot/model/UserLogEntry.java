3ab0bae063fc76f57961642f2b86792a3fafe8a1
RAPHAEL PETERS <RKCPETERS@gmail.com>
2025-11-16 17:44:54 +0000

    After postman testing, made decision to leave UserReflection as object for later expansion

--- src/main/java/com/rkcp/reboot/reboot/model/UserLogEntry.java
+++ src/main/java/com/rkcp/reboot/reboot/model/UserLogEntry.java
@@ -1,4 +1,6 @@
 package com.rkcp.reboot.reboot.model;
 
-public record UserLogEntry(LogType logType, UserReflection userReflection) {
+public record UserLogEntry(
+        LogType logType,
+        UserReflection userReflection) { // UserReflection might grow from just being a string to a full object...
 }
